dbc34ee9b0d360ba14481dbfcd5ecb6be50d1003
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-06-11 20:03:07 +0200

    fix(): import instead of require

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -1,6 +1,11 @@
 /* eslint-disable */
-const fs = require("fs")
-const dotenv = require("dotenv")
+import fs from "fs"
+import dotenv from "dotenv"
+import { fileURLToPath } from "url"
+import { dirname } from "path"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 dotenv.config()
 
