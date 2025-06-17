3213854c088f5ddecb20a8aa217c99498ec61e16
Dante Galeazzi <dantegaleazzi@MacBook-Pro-de-Dante.local>
2025-06-17 12:06:42 +0200

    chore: remove unused __filename and __dirname from prebuild

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -4,9 +4,6 @@ import dotenv from "dotenv"
 import { fileURLToPath } from "url"
 import { dirname } from "path"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
 dotenv.config()
 
 let ENV_CONTENT = {}
