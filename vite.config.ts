45db22a8dd8541f378afff0ccf13fcff916fe347
Gabriel Díaz <gabrieldiaz31@gmail.com>
2025-06-17 11:16:35 -0300

    fix: build command (#9)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 
 // https://vitejs.dev/config/
 // eslint-disable-next-line import/no-default-export
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  const envVariables = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react()],
+    ...(command === "build" ? { base: envVariables.VITE_BASE_URL } : undefined),
+  }
 })
