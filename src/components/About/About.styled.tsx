b0d43478afd9a2f72922a01fe09933d10d434ea0
DevGmStudios2025 <devs@gmstudios.gg>
2025-06-19 18:46:12 +0200

    feat(): styled files for about and faq

---
+++ src/components/About/About.styled.tsx
@@ -0,0 +1,68 @@
+import { styled } from "styled-components"
+import { breakpoints, theme } from "../../utils/theme"
+
+const AboutContainer = styled.div`
+  background-color: ${theme.black};
+  padding: 60px 20px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+
+  @media (min-width: ${breakpoints.md}) {
+    padding: 80px 80px;
+  }
+
+  @media (min-width: ${breakpoints.l}) {
+    flex-direction: row;
+    gap: 0px;
+    align-items: stretch;
+    padding: 100px 20px;
+  }
+`
+
+const TitleAboutSection = styled.h2`
+  font-size: 40px;
+  font-weight: 900;
+  color: ${theme.white};
+  letter-spacing: 0.1em;
+`
+
+const DivVerticalLine = styled.div`
+  width: 2px;
+  background-color: ${theme.white};
+  display: none;
+  margin: 0 40px;
+  height: auto;
+  align-self: stretch;
+
+  @media (min-width: ${breakpoints.l}) {
+    display: block;
+  }
+`
+
+const LiAbout = styled.li`
+  font-size: 22px;
+  font-weight: 500;
+  color: ${theme.white};
+  margin-bottom: 5px;
+`
+
+const SpanAbout = styled.span`
+  font-size: 22px;
+  font-weight: 900;
+  background: linear-gradient(90deg, ${theme.purple}, ${theme.orange});
+  -webkit-background-clip: text;
+  background-clip: text;
+  color: transparent;
+`
+
+export {
+  AboutContainer,
+  DivVerticalLine,
+  LiAbout,
+  SpanAbout,
+  TitleAboutSection,
+}
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,6 +1,11 @@
-import { styled } from "styled-components"
 import { useResizePage } from "../../hooks/useResizePage"
-import { breakpoints, theme } from "../../utils/theme"
+import {
+  AboutContainer,
+  DivVerticalLine,
+  LiAbout,
+  SpanAbout,
+  TitleAboutSection,
+} from "./About.styled"
 
 const About = () => {
   const { isMobile } = useResizePage({ size: 992 })
@@ -47,63 +52,5 @@ const About = () => {
     </AboutContainer>
   )
 }
-
-const AboutContainer = styled.div`
-  background-color: ${theme.black};
-  padding: 60px 20px;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 20px;
-
-  @media (min-width: ${breakpoints.md}) {
-    padding: 80px 80px;
-  }
-
-  @media (min-width: ${breakpoints.l}) {
-    flex-direction: row;
-    gap: 0px;
-    align-items: stretch;
-    padding: 100px 20px;
-  }
-`
-
-const TitleAboutSection = styled.h2`
-  font-size: 40px;
-  font-weight: 900;
-  color: ${theme.white};
-  letter-spacing: 0.1em;
-`
-
-const DivVerticalLine = styled.div`
-  width: 2px;
-  background-color: ${theme.white};
-  display: none;
-  margin: 0 40px;
-  height: auto;
-  align-self: stretch;
-
-  @media (min-width: ${breakpoints.l}) {
-    display: block;
-  }
-`
-
-const LiAbout = styled.li`
-  font-size: 22px;
-  font-weight: 500;
-  color: ${theme.white};
-  margin-bottom: 5px;
-`
-
-const SpanAbout = styled.span`
-  font-size: 22px;
-  font-weight: 900;
-  background: linear-gradient(90deg, ${theme.purple}, ${theme.orange});
-  -webkit-background-clip: text;
-  background-clip: text;
-  color: transparent;
-`
 
 export { About }
