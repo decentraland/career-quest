89ea2d402e1c9d083ae168975dd33b1a3c17480c
DevGmStudios2025 <devs@gmstudios.gg>
2025-06-24 22:15:44 +0200

    feat(): live talks component

--- src/components/LiveTalks/LiveTalks.styled.tsx
+++ src/components/LiveTalks/LiveTalks.styled.tsx
@@ -1,147 +1,5 @@
 import { styled } from "styled-components"
-import { breakpoints } from "../../utils/theme"
-
-const LiveTalksContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  color: #ebecfa;
-  background-repeat: no-repeat;
-  background-size: cover;
-  padding: 24px;
-  position: relative;
-  max-width: 1200px;
-  margin-block: 100px;
-  @media (min-width: ${breakpoints.l}) {
-    flex-direction: row;
-    justify-content: center;
-    gap: 24px;
-    align-items: flex-start;
-  }
-
-  &::before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    z-index: 1;
-  }
-
-  & > * {
-    position: relative;
-    z-index: 2;
-  }
-
-  @media (max-width: ${breakpoints.md}) {
-    padding: 30px 15px;
-  }
-
-  @media (max-width: ${breakpoints.s}) {
-    padding: 20px 10px;
-  }
-
-  .live-talks-header {
-    gap: 10px;
-    padding-left: 20px;
-    border-left: 1px solid #ebecfa;
-    margin-block: 24px;
-    height: 100%;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-
-    h2 {
-      font-size: 32px;
-      font-weight: 700;
-      color: #ebecfa;
-      letter-spacing: 0.04em;
-      text-align: left;
-    }
-
-    h5 {
-      font-size: 18px;
-      font-weight: 400;
-      color: #ebecfa;
-      letter-spacing: 0.06em;
-      text-align: left;
-      margin-block: 12px;
-      max-width: 360px;
-
-      a {
-        text-decoration: underline;
-        color: rgb(88, 122, 235);
-      }
-    }
-
-    @media (min-width: ${breakpoints.l}) {
-      max-width: 360px;
-      text-align: right;
-      align-items: flex-end;
-
-      h5 {
-        text-align: right;
-        max-width: 350px;
-      }
-
-      h2 {
-        text-align: right;
-      }
-    }
-
-    @media (min-width: ${breakpoints.l}) {
-      margin-block: 0;
-      padding-left: 0;
-      border-left: none;
-      height: 100%;
-    }
-
-    > div.header-desktop {
-      margin-top: 24px;
-      width: 100%;
-      display: none;
-
-      > img:first-child {
-        margin-bottom: 12px;
-      }
-
-      @media (min-width: 1024px) {
-        display: flex;
-        align-items: flex-end;
-        justify-content: flex-end;
-        text-align: right;
-        flex-direction: column;
-        display: flex;
-      }
-
-      p {
-        font-size: 14px;
-        font-weight: 400;
-        color: #ebecfa;
-        max-width: 350px;
-        letter-spacing: 0.06em;
-        margin-bottom: 24px;
-      }
-
-      .header-desktop-icons {
-        align-items: flex-end;
-        justify-content: flex-end;
-
-        svg {
-          width: 240px;
-          height: 100%;
-          margin-left: auto;
-        }
-      }
-
-      a:first-child {
-        margin-top: 24px;
-      }
-    }
-  }
-`
+import { breakpoints, theme } from "../../utils/theme"
 
 const ModalTalkImage = styled.img`
   width: 100%;
@@ -150,38 +8,84 @@ const ModalTalkImage = styled.img`
   margin: 0 auto;
 `
 
-const TalksGrid = styled.div`
-  display: grid;
-  gap: 30px;
+const LiveTalksContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: ${theme.black};
+  padding: 60px 20px;
   width: 100%;
-  max-width: 1240px;
-  margin-bottom: 60px;
-
-  @media (max-width: ${breakpoints.s}) {
-    grid-template-columns: 1fr;
-    gap: 30px;
-  }
 
   @media (min-width: ${breakpoints.md}) {
-    grid-template-columns: repeat(2, minmax(240px, 1fr));
+    padding: 80px 80px;
+  }
+
+  @media (min-width: ${breakpoints.l}) {
+    flex-direction: row;
+    align-items: flex-start;
     gap: 20px;
-    border-left: 1px solid #ebecfa;
-    padding-left: 24px;
+  }
+`
+
+const LiveTalksCardsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 40px;
+  padding: 20px;
+  width: 100%;
+
+  @media (min-width: ${breakpoints.md}) {
+    grid-template-columns: 1fr 1fr;
+    align-items: stretch;
+  }
+`
+
+const LiveTalksTitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+
+  p {
+    font-size: 24px;
+    font-weight: 500;
+    color: ${theme.white};
+    text-align: end;
+  }
+`
+
+const LiveTalksTitle = styled.h2`
+  font-size: 40px;
+  font-weight: 900;
+  color: ${theme.white};
+  letter-spacing: 0.1em;
+  text-align: center;
+
+  @media (min-width: ${breakpoints.l}) {
+    text-align: end;
   }
 `
 
 const TalkCard = styled.div`
-  color: #ebecfa;
-  text-align: center;
-  transition: transform 0.3s ease;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 20px;
+  background-color: ${theme.black};
+  height: 100%;
 
   img {
     width: 100%;
-    border: 1px solid;
+    max-width: 346px;
+    border: 2px solid ${theme.white};
     object-fit: cover;
-    aspect-ratio: 16/9;
-    border-radius: 20px;
+    border-radius: 25px;
     cursor: pointer;
+
+    aspect-ratio: 16/9;
     transition: transform 0.3s ease;
     will-change: transform;
     &:hover {
@@ -191,76 +95,43 @@ const TalkCard = styled.div`
   }
 
   h3 {
-    margin-top: 15px;
-    font-size: 22px;
-    font-weight: bold;
-    font-family: "Inter", sans-serif !important;
+    font-size: 20px;
+    font-weight: 700;
+    color: ${theme.white};
+    margin-bottom: 10px;
+    text-align: center;
+
+    span {
+      font-size: 16px;
+    }
   }
 
   p {
-    font-size: 14px;
-    font-weight: 600;
-    color: #ebecfa;
-    line-height: 1.4;
+    font-size: 16px;
+    font-weight: 700;
+    color: ${theme.white};
+    text-align: center;
   }
 
-  @media (max-width: ${breakpoints.md}) {
-    padding: 0;
-
-    h3 {
-      font-size: 20px;
-      letter-spacing: 0.05em;
-    }
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
 
     p {
       font-size: 14px;
-      letter-spacing: 0.05em;
-      opacity: 0.8;
+      font-weight: 500;
+      color: ${theme.white};
     }
-
-    img {
-      height: unset;
-    }
-  }
-`
-
-const TalkTime = styled.p`
-  font-weight: 400 !important;
-  letter-spacing: 0.05em;
-`
-
-const TimeInfoTalk = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 10px;
-  font-size: 14px;
-  margin-top: 15px;
-
-  p {
-    color: #ebecfa !important;
-    font-weight: 400 !important;
-    margin: 0;
-    position: relative;
-
-    &:not(:last-child)::after {
-      content: "|";
-      position: absolute;
-      right: -7px;
-    }
-  }
-
-  @media (max-width: ${breakpoints.md}) {
-    font-size: 12px;
-    gap: 8px;
   }
 `
 
 export {
   LiveTalksContainer,
-  ModalTalkImage,
-  TalksGrid,
+  LiveTalksCardsContainer,
+  LiveTalksTitleContainer,
+  LiveTalksTitle,
   TalkCard,
-  TalkTime,
-  TimeInfoTalk,
+  ModalTalkImage,
 }
--- src/components/LiveTalks/LiveTalks.tsx
+++ src/components/LiveTalks/LiveTalks.tsx
@@ -1,75 +1,42 @@
 import { useState } from "react"
 import { liveTalksData } from "./data"
-import iconsTalks from "../../img/vectors/logos-talks.svg?url"
-import iconTalksSingle from "../../img/vectors/talksico.svg?url"
-import { JumpInBtn } from "../JumpInBtn/JumpInBtn"
+import { useResizePage } from "../../hooks/useResizePage"
+import { DivVerticalLine } from "../About/About.styled"
 import { Modal } from "../Modal"
 import {
+  LiveTalksCardsContainer,
   LiveTalksContainer,
+  LiveTalksTitle,
+  LiveTalksTitleContainer,
   ModalTalkImage,
   TalkCard,
-  TalkTime,
-  TalksGrid,
-  TimeInfoTalk,
 } from "./LiveTalks.styled"
 
 const LiveTalks = () => {
+  const { isMobile } = useResizePage({ size: 992 })
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [modalImageUrl, setModalImageUrl] = useState("")
 
   return (
     <>
-      <LiveTalksContainer id="talks">
-        <div className="live-talks-header">
-          <h2>Insider Career Talks</h2>
-          <h5>
-            All talks will be live-streamed in{" "}
-            <a
-              href="https://decentraland.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Decentraland
-            </a>
-            ,{" "}
-            <a
-              href="https://x.com/decentraland
-"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              X
-            </a>
-            ,{" "}
-            <a
-              href="https://www.youtube.com/@decentraland_foundation"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              YouTube
-            </a>{" "}
-            and{" "}
-            <a
-              href="https://www.linkedin.com/company/decentralandorg"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              LinkedIn
-            </a>
-          </h5>
-          <div className="header-desktop">
-            <img src={iconTalksSingle} alt="Talks Single" />
-            <p>
-              All content will repeat in world starting at Midnight UTC the
-              following day
-            </p>
-            <div className="header-desktop-icons">
-              <img src={iconsTalks} alt="LinkedIn" />
-            </div>
-            <JumpInBtn />
-          </div>
-        </div>
-        <TalksGrid>
+      <LiveTalksContainer>
+        {isMobile ? (
+          <LiveTalksTitle>Launch your Web3 Career</LiveTalksTitle>
+        ) : (
+          <>
+            <LiveTalksTitleContainer>
+              <LiveTalksTitle>Insider Career Talks</LiveTalksTitle>
+              <p>
+                All talks will be live-streamed in Decentraland, X, Youtube and
+                Linkedin
+              </p>
+            </LiveTalksTitleContainer>
+
+            <DivVerticalLine />
+          </>
+        )}
+
+        <LiveTalksCardsContainer>
           {liveTalksData.map((talk, index) => (
             <TalkCard key={index}>
               <img
@@ -80,22 +47,101 @@ const LiveTalks = () => {
                   setModalImageUrl(talk.image)
                 }}
               />
-              <h3>{talk.title}:</h3>
-              <p>{talk.description}</p>
+              <h3>
+                {talk.title}: <span>{talk.description}</span>
+              </h3>
               <p>{talk.date}</p>
-              <TimeInfoTalk>
-                <TalkTime>{talk["time-start"]}</TalkTime>
-                <TalkTime>{talk["time-start-p"]}</TalkTime>
-                {/* <TalkTime>{talk["time-end"]}</TalkTime> */}
-              </TimeInfoTalk>
+              <div>
+                <p>{talk["time-start"]}</p>
+                <p>{talk["time-start-p"]}</p>
+              </div>
             </TalkCard>
           ))}
-        </TalksGrid>
+        </LiveTalksCardsContainer>
       </LiveTalksContainer>
       <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
         <ModalTalkImage src={modalImageUrl} alt="Modal Image" />
       </Modal>
     </>
+
+    //     <>
+    //       <LiveTalksContainer id="talks">
+    //         <div className="live-talks-header">
+    //           <h2>Insider Career Talks</h2>
+    //           <h5>
+    //             All talks will be live-streamed in{" "}
+    //             <a
+    //               href="https://decentraland.org/"
+    //               target="_blank"
+    //               rel="noopener noreferrer"
+    //             >
+    //               Decentraland
+    //             </a>
+    //             ,{" "}
+    //             <a
+    //               href="https://x.com/decentraland
+    // "
+    //               target="_blank"
+    //               rel="noopener noreferrer"
+    //             >
+    //               X
+    //             </a>
+    //             ,{" "}
+    //             <a
+    //               href="https://www.youtube.com/@decentraland_foundation"
+    //               target="_blank"
+    //               rel="noopener noreferrer"
+    //             >
+    //               YouTube
+    //             </a>{" "}
+    //             and{" "}
+    //             <a
+    //               href="https://www.linkedin.com/company/decentralandorg"
+    //               target="_blank"
+    //               rel="noopener noreferrer"
+    //             >
+    //               LinkedIn
+    //             </a>
+    //           </h5>
+    //           <div className="header-desktop">
+    //             <img src={iconTalksSingle} alt="Talks Single" />
+    //             <p>
+    //               All content will repeat in world starting at Midnight UTC the
+    //               following day
+    //             </p>
+    //             <div className="header-desktop-icons">
+    //               <img src={iconsTalks} alt="LinkedIn" />
+    //             </div>
+    //             <JumpInBtn />
+    //           </div>
+    //         </div>
+    //         <TalksGrid>
+    //           {liveTalksData.map((talk, index) => (
+    //             <TalkCard key={index}>
+    //               <img
+    //                 src={talk.image}
+    //                 alt={talk.title}
+    //                 onClick={() => {
+    //                   setIsModalOpen(true)
+    //                   setModalImageUrl(talk.image)
+    //                 }}
+    //               />
+    //               <h3>{talk.title}:</h3>
+    //               <p>{talk.description}</p>
+    //               <p>{talk.date}</p>
+    //               <TimeInfoTalk>
+    //                 <TalkTime>{talk["time-start"]}</TalkTime>
+    //                 <TalkTime>{talk["time-start-p"]}</TalkTime>
+    //                 {/* <TalkTime>{talk["time-end"]}</TalkTime> */}
+    //               </TimeInfoTalk>
+    //             </TalkCard>
+    //           ))}
+    //         </TalksGrid>
+    //       </LiveTalksContainer>
+    //       <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+    //         <ModalTalkImage src={modalImageUrl} alt="Modal Image" />
+    //       </Modal>
+    //     </>
   )
 }
 
