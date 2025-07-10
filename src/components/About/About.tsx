76f71cf55caf10e83aa37020c982d2cb99446f80
DevGmStudios2025 <devs@gmstudios.gg>
2025-07-10 17:01:15 +0200

    refactor(): w minuscula

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -45,7 +45,7 @@ const About = () => {
 
           <LiAbout>
             Connect with{" "}
-            <SpanAbout>Bondex, Web3.career, Metana and SheFi</SpanAbout> for
+            <SpanAbout>Bondex, web3.career, Metana and SheFi</SpanAbout> for
             hiring and training insights
           </LiAbout>
 
